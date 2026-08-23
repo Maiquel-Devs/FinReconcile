c4ab52fad22522c44b363894e0d48d00f84f51e7
Maiquel <junior.mafra13062005@gmail.com>
2026-08-23 15:51:52 -0300

    chore: clean up boilerplate templates and unused view files
    - Remove unused HomeController and Home views
    - Remove default ErrorViewModel and Error.cshtml view
    - Remove default UnitTest1.cs boilerplate file
    - Verify clean build and green status on test suite

--- FinReconcile/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace FinReconcile.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- tests/FinReconcile.UnitTests/obj/Debug/net10.0/FinReconcile.UnitTests.AssemblyInfo.cs
+++ tests/FinReconcile.UnitTests/obj/Debug/net10.0/FinReconcile.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinReconcile.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd476906c92bb922bdb4eb5cbcd54af5600ce6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54ba6818aea6079708d667ff6398f6e2de40996")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinReconcile.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinReconcile.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
